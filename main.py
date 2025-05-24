f91c2e3aa83e00932e2e8d1c7455c2876850e45f
Abhishek <103832942+cosmos-dx@users.noreply.github.com>
2025-05-24 10:31:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,4 +48,8 @@ def upload_resume():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host=os.getenv('HOST', '0.0.0.0'), 
+        port=int(os.getenv('PORT', '8080')),
+        debug=False
+    )
